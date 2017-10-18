54d40c509874b856cea8b69f03f032c8a34557f7
staffonechristian <christianstaffone@gmail.com>
2017-10-18 12:15:15 -0400

    first basic login

--- app/src/main/java/com/example/staffonechristian/seeksubstitute/LoginActivity.java
+++ app/src/main/java/com/example/staffonechristian/seeksubstitute/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.example.staffonechristian.seeksubstitute;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +13,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -66,6 +71,7 @@ public class LoginActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                CheckExist(emailS);
                                 startActivity(intent);
 
                             } else {
@@ -85,5 +91,24 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void CheckExist(final String emailS) {
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists())
+                {
+                    DatabaseReference writeDb = FirebaseDatabase.getInstance().getReference();
+                    writeDb.child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(emailS);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
